45c3d3ccf763fff0ddc5f38f03a327daacd08b16
harryclark <33656495+harryclark@users.noreply.github.com>
2018-11-08 14:54:51 +0000

    Add files via upload
    Added bookmarking, registered by shake or tap with basic visual display

--- speechRecognition.js
+++ speechRecognition.js
@@ -1,29 +1,54 @@
-window.addEventListener('load', init);
+
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
-var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral'];
-var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
 
+heard = [];
+highlights = [];
 let speechAPI = new SpeechRecognition();
 let speechRecognitionList = new SpeechGrammarList();
+speechAPI.continuous = true;
 speechAPI.grammars = speechRecognitionList;
 speechAPI.lang = 'en-US';
 speechAPI.interimResults = false;
 speechAPI.maxAlternatives = 1
 
-function init(){
 
-  document.getElementById('RecordBtn').addEventListener('click', startRecording);
-};
+function toggleRecording(){
+  let button = document.getElementById("RecordBtn");
+  if(button.innerHTML == "Start Listening"){
+    speechAPI.start();
+    button.innerHTML = "Stop Listening";
+    button.style.backgroundColor = "blue";
+    console.log("RecordingStarted");
+  }
+  else{
+    speechAPI.stop();
+    button.innerHTML = "Start Listening";
+    button.style.backgroundColor = "red";
+    console.log("RecordingStopped");
+  }
+}
 
-function startRecording(){
-  speechAPI.start();
-  console.log("RecordingStarted");
-};
+function addBookmark(){
+  if(!highlights.includes(heard.length)){
+    highlights.push(heard.length);
+  }
+}
 
 speechAPI.onresult = function(e){
   let last = e.results.length -1;
   let result = e.results[last][0].transcript;
+  heard.push(result);
+  let textpush = [];
+  for(i=0; i<=heard.length; i++){
+    if(highlights.includes(i)){
+      textpush.push("<span class=marked>" + heard[i] + "</span>");
+    }
+    else{
+      textpush.push(heard[i]);
+    }
+  }
+  document.getElementById("textArea").innerHTML = textpush.join(" ");
   console.log(result);
 }
